610f731f9c67999c16d3c212d1f8fcfc6dbe34ca
Patrick Silva <martins.patrick3@gmail.com>
2023-11-26 13:34:18 -0400

    fix: adjust colation database charset

--- config.php
+++ config.php
@@ -4,13 +4,4 @@
   $db_pass = 'root1001';
   $db_name = 'book_store';
 
-  try{
-    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    if ($mysqli->connect_error) {
-      die("Falha na conexão com o banco de dados: " . $mysqli->connect_error);
-    }
-    } catch (PDOException $e){
-    echo 'Erro na conexão: '.$e->getMessage().'<br>';
-    echo 'Codigo do erro: '.$e->getCode();
-  }
-?>
+  $mysqli->set_charset('utf8');
